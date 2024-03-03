24da8b977d8a84bbfb1d8a84a51ef32e83db5c93
Neptune <neptune650@proton.me>
2024-03-03 14:29:06 -0500

    UltimMC: Fix wrapper script

--- ultimmc/opt/ultimmc/run.sh
+++ ultimmc/opt/ultimmc/run.sh
@@ -8,8 +8,9 @@ deploy() {
 
     wget --progress=dot:force "https://nightly.link/UltimMC/Launcher/workflows/main/develop/mmc-cracked-lin64.zip" 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-close --auto-kill --title="Downloading UltimMC..."
 
-    tar -xzf ${PACKAGE} --transform='s,UltimMC/,,'
-    rm ${PACKAGE}
+    unzip mmc-cracked-lin64.zip
+    mv -rfv UltimMC/* .
+    rm mmc-cracked-lin64.zip
     chmod +x UltimMC
 }
 
